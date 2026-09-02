ab9cd9e91b40d38b5ba7ea945d57a0ef4f55ccfd
Phalla Lanh <lanh.phalla26@gmail.com>
2026-09-02 14:53:27 +0700

    Update Login Response

--- profile/src/main/kotlin/user/profile/authentication/login/loginDTO/LoginResponseDTO.kt
+++ profile/src/main/kotlin/user/profile/authentication/login/loginDTO/LoginResponseDTO.kt
@@ -1,10 +1,14 @@
 package user.profile.authentication.login.loginDTO
 
+import user.profile.permission.dto.PermissionResponse
+import user.profile.role.dto.RoleResponse
+
 data class LoginResponseDTO(
     val status: String,
     val code: Int,
     val message: String,
     val username: String,
-    val role: String,
+    val role: RoleResponse?,
+    val permissions: List<PermissionResponse>,
     val token: String? = null
 )
--- profile/src/main/kotlin/user/profile/authentication/login/service/LoginService.kt
+++ profile/src/main/kotlin/user/profile/authentication/login/service/LoginService.kt
@@ -6,14 +6,20 @@ import user.profile.configJWT.JwtService
 import user.profile.authentication.login.loginDTO.LoginRequestDTO
 import user.profile.authentication.login.loginDTO.LoginResponseDTO
 import user.profile.authentication.login.repository.LoginRepository
+import user.profile.permission.dto.PermissionResponse
+import user.profile.role.dto.RoleResponse
+import user.profile.rolepermission.RolePermissionRepository
 
 @Service
 class LoginService (
     private val loginRepository: LoginRepository,
     private val passwordEncoder: PasswordEncoder,
-    private val jwtService: JwtService
+    private val jwtService: JwtService,
+    private val rolePermissionRepository: RolePermissionRepository
 ) {
-    fun login(request: LoginRequestDTO): LoginResponseDTO {
+    fun login(
+        request: LoginRequestDTO):
+            LoginResponseDTO {
 
         // Find user
         val user = loginRepository.findByUsername(request.username) ?: return LoginResponseDTO(
@@ -21,7 +27,8 @@ class LoginService (
             code = 401,
             message = "Invalid username",
             username = request.username,
-            role = ""
+            role = null,
+            permissions = emptyList()
         )
 
         // Check password
@@ -31,18 +38,44 @@ class LoginService (
                 code = 401,
                 message = "Invalid password",
                 username = request.username,
-                role = ""
+                role = null,
+                permissions = emptyList()
             )
         }
 
         // Get role
-        val roleName = user.role?.name?:""
+        val role: RoleResponse? = user.role?.let {
+            RoleResponse(
+                id = it.id!!,
+                name = it.name!!
+            )
+        }
+
+        // Get permissions from RolePermission
+        val permissions: List<PermissionResponse> =
+            if (user.role?.id != null) {
+
+                rolePermissionRepository
+                    .findByRoleId(user.role!!.id!!)
+                    .mapNotNull { rolePermission ->
+
+                        rolePermission.permission?.let { permission ->
+                            PermissionResponse(
+                                id = permission.id!!,
+                                name = permission.name
+                            )
+                        }
+                    }
+
+            } else {
+                emptyList()
+            }
 
         // Generate JWT token
         val token = jwtService.generateToken(
             userId = user.id!!,
             username = user.username!!,
-            role = roleName
+            role = "",
         )
 
 
@@ -53,7 +86,8 @@ class LoginService (
             code = 200,
             message = "Login successfully",
             username = request.username,
-            role = "",
+            role = role,
+            permissions = permissions,
             token = token
         )
     }
