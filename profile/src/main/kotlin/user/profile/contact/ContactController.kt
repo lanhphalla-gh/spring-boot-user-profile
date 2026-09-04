9129ed011aede5a833b9f7a308ffda5176745404
Phalla Lanh <lanh.phalla26@gmail.com>
2026-09-04 08:52:46 +0700

    Update RequestMapping contact admin

--- profile/src/main/kotlin/user/profile/contact/ContactController.kt
+++ profile/src/main/kotlin/user/profile/contact/ContactController.kt
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/api/contactadmin")
+@RequestMapping("/api/contact-admin")
 class ContactController(
     private val contactEmailService: ContactEmailService
 ) {
